6e47eb24f452b12e638ff03b19e70b2213b4ef0e
Harry Fitzgerald <harry.f@elanco.com>
2021-03-18 16:17:42 +0000

    more changes

--- index.js
+++ index.js
@@ -4,8 +4,49 @@ const depcheck = require('depcheck')
 
 const bullets = line => `* ${line}`
 
+const options = {
+  ignoreBinPackage: false, // ignore the packages with bin entry
+  skipMissing: false, // skip calculation of missing dependencies
+  ignorePatterns: [
+    // files matching these patterns will be ignored
+    'sandbox',
+    'dist',
+    'bower_components',
+  ],
+  ignoreMatches: [
+    // ignore dependencies that matches these globs
+    'grunt-*',
+  ],
+  parsers: {
+    // the target parsers
+    '**/*.js': depcheck.parser.es6,
+    '**/*.jsx': depcheck.parser.jsx,
+  },
+  detectors: [
+    // the target detectors
+    depcheck.detector.requireCallExpression,
+    depcheck.detector.importDeclaration,
+  ],
+  specials: [
+    // the target special parsers
+    depcheck.special.eslint,
+    depcheck.special.webpack,
+  ],
+  package: {
+    // may specify dependencies instead of parsing package.json
+    dependencies: {
+      lodash: '^4.17.15',
+    },
+    devDependencies: {
+      eslint: '^6.6.0',
+    },
+    peerDependencies: {},
+    optionalDependencies: {},
+  },
+}
+
 async function main() {
-	const {dependencies, devDependencies} = await depcheck('.')
+	const {dependencies, devDependencies} = await depcheck('.', options)
 	const token = core.getInput('GITHUB_TOKEN')
 	const {context} = github
 	if (!context.payload.pull_request) {
